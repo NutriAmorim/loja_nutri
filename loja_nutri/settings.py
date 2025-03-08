6df4c1376974cef5b6491ed780219048bd4b8d86
marcos <nutriamorim69@gmail.com>
2025-03-08 12:53:55 -0300

    Move base.html para a pasta correta e atualiza configurações

--- loja_nutri/settings.py
+++ loja_nutri/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'loja_nutri.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [str(BASE_DIR / 'loja_app' / 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'loja_app', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
