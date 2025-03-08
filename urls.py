68c0f858145d28f8f655d0cdfe88238427047d83
marcos <nutriamorim69@gmail.com>
2025-03-08 09:36:53 -0300

    Adicionando links no menu

--- urls.py
+++ urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from loja_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.boas_vindas, name='boas_vindas'),  # Página inicial
-    path('receitas_medicinais/', views.receitas_medicinais, name='receitas_medicinais'),
-    path('pesquisas_cientificas/', views.pesquisas_cientificas, name='pesquisas_cientificas'),
-    path('quem_sou_eu/', views.quem_sou_eu, name='quem_sou_eu'),
-    path('quem_somos_nos/', views.quem_somos_nos, name='quem_somos_nos'),
+    path('', include('loja_app.urls')),  # Inclui as URLs do app loja_app
 ]
