2c3b735d5c055b4687cca62f790a58c9ffa651a1
marcos <nutriamorim69@gmail.com>
2025-03-08 09:57:42 -0300

    Corrigindo caminhos de templates

--- loja_app/views.py
+++ loja_app/views.py
@@ -4,13 +4,13 @@ def boas_vindas(request):
     return render(request, 'loja_app/boas_vindas.html')
 
 def receitas_medicinais(request):
-    return render(request, 'receitas_medicinais.html')
+    return render(request, 'loja_app/receitas_medicinais.html')
 
 def pesquisas_cientificas(request):
-    return render(request, 'pesquisas_cientificas.html')
+    return render(request, 'loja_app/pesquisas_cientificas.html')
 
 def quem_sou_eu(request):
-    return render(request, 'quem_sou_eu.html')
+    return render(request, 'loja_app/quem_sou_eu.html')
 
 def quem_somos_nos(request):
-    return render(request, 'quem_somos_nos.html')
+    return render(request, 'loja_app/quem_somos_nos.html')
